685894fd3f7a650df7259ad6b40afc1c5db625fa
weeabookstore <34562292+weeabookstore@users.noreply.github.com>
2020-04-17 02:19:27 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -381,7 +381,7 @@ var trainees = [];
 var filteredTrainees = [];
 // holds the ordered list of rankings that the user selects
 var ranking = newRanking();
-const rowNums = [1, 2, 4, 5];
+const rowNums = [1, 2, 3, 4, 5];
 //window.addEventListener("load", function () {
   populateRanking();
   readFromCSV("./trainee_info.csv");
